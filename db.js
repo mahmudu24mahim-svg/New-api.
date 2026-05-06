2afc9dca4ab77c36524bc7f547b8931baa93f352
mahmudu24mahim-svg <mahmudu24mahim@gmail.com>
2026-05-06 09:17:09 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   key: { type: String, unique: true },
   label: String,
   limit: Number,
   used: { type: Number, default: 0 },
-  expiry: Number,
-  createdAt: { type: Number, default: Date.now }
+  expiry: Number
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
